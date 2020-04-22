7a3ce03798e2ac545cc34ada46a280e4985f93fd
linboxuan <377760330@qq.com>
2020-04-22 23:22:53 +0800

    axios request baseURL

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-04-16 18:31:45
- * @LastEditTime: 2020-04-16 18:33:41
+ * @LastEditTime: 2020-04-22 23:01:06
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /api/Users/linboxuan/vscodeProjects/vue-blog/src/utils/request.js
@@ -9,7 +9,7 @@
 import axios from 'axios'
 
 const service = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://39.97.208.148:3000/',
     timeout: 5000
 })
 
